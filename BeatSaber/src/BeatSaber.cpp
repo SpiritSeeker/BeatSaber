dfc6183e933867324756ee6e059e7be859323140
SpiritSeekerMac <sanjeet029@gmail.com>
2021-08-04 12:34:40 +0530

    Compilation environment setup

---
+++ BeatSaber/src/BeatSaber.cpp
@@ -0,0 +1,20 @@
+#include <Ancora.h>
+#include <Ancora/Core/EntryPoint.h>
+
+class BeatSaber : public Ancora::Application
+{
+public:
+	BeatSaber()
+	{
+	}
+
+	~BeatSaber()
+	{
+	}
+
+};
+
+Ancora::Application* Ancora::CreateApplication()
+{
+	return new Sandbox();
+}
---
+++ premake5.lua
@@ -0,0 +1,102 @@
+workspace "BeatSaber"
+	architecture "x64"
+	startproject "BeatSaber"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	platforms
+	{
+		"linux",
+		"windows",
+		"macosx"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "Ancora/vendor/GLFW/include"
+IncludeDir["Glad"] = "Ancora/vendor/Glad/include"
+IncludeDir["ImGui"] = "Ancora/vendor/imgui"
+IncludeDir["glm"] = "Ancora/vendor/glm"
+IncludeDir["stb_image"] = "Ancora/vendor/stb_image"
+IncludeDir["assimp"] = "Ancora/vendor/assimp/include"
+
+include "AncoraEngine"
+
+project "BeatSaber"
+	location "BeatSaber"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"AncoraEngine/Ancora/vendor/spdlog/include",
+		"AncoraEngine/Ancora/src",
+		"BeatSaber/src",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.assimp}"
+	}
+
+	links
+	{
+		"Ancora",
+		"assimp"
+	}
+
+	filter "system:linux"
+
+		defines
+		{
+			"AE_PLATFORM_LINUX",
+			"AE_BUILD_DLL"
+		}
+
+		links
+		{
+			"pthread"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"AE_PLATFORM_WINDOWS"
+		}
+
+
+	filter "configurations:Debug"
+		defines
+		{
+			"AE_DEBUG",
+			"AE_ENABLE_ASSERTS"
+		}
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "AE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "AE_DIST"
+		runtime "Release"
+		optimize "on"
